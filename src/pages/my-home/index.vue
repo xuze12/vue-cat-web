53838216c4c88076849f319f1e27ca0f64365689
xuze12 <504171012@qq.com>
2020-09-29 15:10:36 +0800

    初始化

--- src/pages/my-home/index.vue
+++ src/pages/my-home/index.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="hello">
-    <div class="header"></div>
+    <div class="header">
+      <div class="headset">
+        <img
+          class="headset-img"
+          src="../../assets/icon/headset.svg"
+          alt="Headset"
+        />
+      </div>
+
+      <div class="info">
+        <div class="info-left">
+          <img
+            class="info-head-portrait"
+            src="../../assets/images/head-portrait.jpeg"
+          />
+          <div class="info-account">
+            <div class="info-name">傲视小可</div>
+            <div class="info-">猫号:123456</div>
+          </div>
+        </div>
+
+        <div class="personal-button">个人资料></div>
+      </div>
+    </div>
     <van-button type="primary">主要按钮</van-button>
-    <div class="box"></div>
   </div>
 </template>
 
@@ -19,10 +41,9 @@ export default {
       msg: ''
     }
   },
-  mounted: {
-    add () {
-      console.log(12)
-    }
+  mounted () {},
+  methods: {
+    add () {}
   }
 }
 </script>
@@ -30,12 +51,36 @@ export default {
 <style scoped>
 .header {
   width: 100%;
-  height: 20.875rem;
+  height: 10.4375rem;
   background-color: #33c9ae;
 }
-.box{
-  width: 12.5rem;
-  height: 12.5rem;
-  background-color:red;
+.headset {
+  height: 2.1875rem;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 0.78125rem;
+}
+.headset-img {
+  width: 1.09375rem;
+  height: 1.09375rem;
+}
+.info {
+  height: 4.6875rem;
+  padding-left: 0.9375rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.info-left{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.info-head-portrait {
+  width: 4.0625rem;
+  height: 4.0625rem;
+  border-radius: 50%;
+  overflow: hidden;
 }
 </style>
